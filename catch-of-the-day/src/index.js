24fac9fc94217c489f53b949673ad37a13292a2e
Jake Franklin <jacob.d.franklin@gmail.com>
2017-12-18 13:22:10 -0700

    Videos 1-11

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,22 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import {BrowserRouter, Match, Miss } from 'react-router';
+
+import './css/style.css';
+
+import App from './components/App.js';
+import notFound from './components/notFound.js'
+import StorePicker from './components/StorePicker.js';
+
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={StorePicker} />
+                <Match exactly pattern="/store/:storeId" component={App} />
+                <Miss component={notFound} />
+            </div>
+        </BrowserRouter>
+    )
+}
+render(<Root/>, document.getElementById("main"));
